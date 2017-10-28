287fb31333752b975c06ce22303dc832a6d0ea66
Joshua Ernzen <josh@elem14.tech>
2017-10-27 21:22:44 -0500

    Initial Commit

---
+++ cat/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cat
+//
+//  Created by Joshua Ernzen on 10/27/17.
+//  Copyright © 2017 Joshua Ernzen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
